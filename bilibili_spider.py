7052f7b2a82b5f54fd8249f26024aa1c19b916af
odd zhang <36219478+zzhjiyin@users.noreply.github.com>
2021-05-20 22:17:39 +0800

    Add files via upload
    解析json
    插入sql
    更新sql
    3个基础功能

---
+++ bilibili_spider.py
@@ -0,0 +1,109 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from bilibili_api import video
+import re
+import csv
+import traceback
+import  time
+import pymysql
+
+headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+    }
+# url="https://space.bilibili.com/20927?from=search&seid=15325431671173261800"
+#
+# r = requests.get(url, headers=headers)
+# html = r.content.decode()
+# soup = BeautifulSoup(html,'lxml')
+#
+# v = video.get_video_info(bvid="BV1n64y127w4")
+# # print(json.dumps(v, indent=4, ensure_ascii=False))
+# json_str = json.dumps(v)
+# pattern = re.compile(r'BV.{10}')
+# result = pattern.findall(json_str)
+# # print(result)
+# print(v)
+results=[]
+title_list=[]
+bvid_list=[]
+url="https://api.bilibili.com/x/space/arc/search?mid=20927&pn=1&ps=25&index=1&jsonp=jsonp"
+r = requests.get(url, headers=headers)
+html = r.content.decode()
+jsont=  r.json()
+results= jsont['data']['list']['vlist']
+for i in results:
+    if bool(re.search("美国单曲周榜", i['title'])) == True:
+        bvid_list.append(i['bvid'])
+        title_list.append(i['title'])
+sql_list = list(zip(bvid_list,title_list))
+print(sql_list)
+# print(bvid_list)
+# print(title_list)
+# for i in range(0,len(sql_list)):
+#     print(sql_list[i][0],"%",sql_list[i][1])
+
+def get_conn():
+    """
+    :return: 连接，游标
+    """
+    # 创建连接
+    conn = pymysql.connect(host="127.0.0.1",
+                           user="root",
+                           password="4zzhjiyin",
+                           db="bilibili",
+                           charset="utf8")
+    # 创建游标
+    cursor = conn.cursor()  # 执行完毕返回的结果集默认以元组显示
+    return conn, cursor
+
+
+def close_conn(conn, cursor):
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
+
+def insert_history():
+    """
+        插入历史数据
+    :return:
+    """
+    cursor = None
+    conn = None
+    try:
+        print(f"{time.asctime()}开始插入数据")
+        conn, cursor = get_conn()
+        sql = "insert into web(bvid,title) values (%s,%s)"
+        for i in range(0,len(sql_list)):
+            cursor.execute(sql,[sql_list[i  ][0],sql_list[i][1]])
+            print(sql_list[i][0]," ",sql_list[i][1])
+        conn.commit()
+        print(f"{time.asctime()}插入完毕")
+    except:
+        traceback.print_exc()
+    finally:
+        close_conn(conn, cursor)
+def update_details():
+    """
+    更新 表
+    :return:
+    """
+    cursor = None
+    conn = None
+    try:
+        conn, cursor = get_conn()
+        sql = "truncate web"
+        cursor.execute(sql)
+        print(f"{time.asctime()}开始更新最新数据")
+        sql = "insert into web(bvid,title) values (%s,%s)"
+        for i in range(0,len(sql_list)):
+            cursor.execute(sql,[sql_list[i][0],sql_list[i][1]])
+            print(sql_list[i][0]," ",sql_list[i][1])
+        conn.commit()  # 提交事务 update delete insert操作
+        print(f"{time.asctime()}更新最新数据完毕")
+    except:
+        traceback.print_exc()
+    finally:
+        close_conn(conn, cursor)
+# update_details()
